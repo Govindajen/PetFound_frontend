e0d962e8df7761856e1cf5d83b979fb67ddaafa1
Govindarajen <coodien21@gmail.com>
2024-07-06 22:57:06 +0200

    Metadata edit / mobile version (menu hamburger 20%

--- src/components/Header.js
+++ src/components/Header.js
@@ -83,11 +83,76 @@ if(openHamburger !== false) {
                 </div>
 
                 <div className={styles.hamburger} style={{'display': hamburgerDisplay}}>
-                    <span className={styles.hamburgerIcon}>
-                        <FontAwesomeIcon icon={faXmark} onClick={ () => {setOpenHamburger(!openHamburger)}} />
-                    </span>
-                    <p>.</p>
-                </div>
+                    <div className={styles.logoContainer}>
+                        <div className={styles.logo}>
+                                <p className={styles.petText}>Pet 
+                                    <span className={styles.foundText}> Found</span>
+                                </p>
+                        </div>
+                            <div className={styles.barIconClose}>
+                                <FontAwesomeIcon icon={faBars} onClick={ () => {setOpenHamburger(!openHamburger)}}/>
+                            </div>
+                    </div>
+
+
+                    <div className={styles.dedicateBTNS}>
+                         <button className={styles.btnFound}>Found</button>
+                         <button className={styles.btnLost}>Lost</button>
+                     </div>
+
+                    <div className={styles.searchBarDiv}>
+                        <select id="pet-select" className={styles.selectBtn}>
+                            <option value="announce">Announce</option>
+                            <option value="user">User</option>
+                        </select>
+
+                        <input 
+                         className={styles.searchInput}
+                            type="search" 
+                            id="site-search" 
+                            name="q" 
+                            placeholder='Announce/username'
+                            value={''} 
+                            />
+                        <button className={styles.searchBtn}>{lang.header.searchBar.btn}</button>
+                    </div>
+
+                        <div className={styles.profileContainerHamburger}>
+                            <div className={styles.usernameDiv}>
+                                {account.username ? (
+                                    <>
+
+                                    <div>
+                                    <Link href={`/profile/${encodeURIComponent(account.username)}`}>
+                                        <span className={styles.userIconSignIn}>
+                                            <FontAwesomeIcon icon={faCircleUser} className={styles.userIcon} />
+                                        </span>
+                                    </Link>
+                                    </div>
+
+                                    <div>
+                                        <p className={styles.welcome}>Profile</p>
+                                        <Link href={`/profile/${encodeURIComponent(account.username)}`} className={styles.username}>
+                                            @{account.username}
+                                        </Link>
+                                    </div>
+                                    </>
+                                ) : (
+                                    <div>
+                                        <p className={styles.welcome}>SignIn | SignUp</p>
+                                        <FontAwesomeIcon
+                                            icon={faCircleUser}
+                                            className={styles.userIcon}
+                                            onClick={handleUserIconClick}
+                                            />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                            <div>
+                                {langDiv}
+                            </div>
+                </div> {/* END OF HAMBURGER DIV */}
 
 
                 <div className={styles.searchBarContainer}>
